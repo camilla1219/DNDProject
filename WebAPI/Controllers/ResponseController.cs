9e56e1a46894b42ad3a72d9d9d9b72566aa710df
liamDND <316346@via.dk>
2024-10-17 15:27:38 +0200

    Update ResponseController.cs

--- WebAPI/Controllers/ResponseController.cs
+++ WebAPI/Controllers/ResponseController.cs
@@ -1,132 +1,116 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
-namespace TodoApi.Controllers
+namespace TodoApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ResponsesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TodoItemsController : ControllerBase
+    private readonly SurveyContext _context;
+
+    public ResponsesController(SurveyContext context)
     {
-        private readonly TodoContext _context;
-
-        public TodoItemsController(TodoContext context)
-        {
-            _context = context;
-        }
-
-        
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<TodoItemDTO>>> GetTodoItems()
-        {
-            var todoItems = await _context.TodoItems
-                .Select(x => ItemToDTO(x))
-                .ToListAsync();
-
-            if (todoItems == null || !todoItems.Any())
-            {
-                return NoContent(); 
-            }
-
-            return Ok(todoItems); 
-        }
-
-        
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TodoItemDTO>> GetTodoItem(long id)
-        {
-            var todoItem = await _context.TodoItems.FindAsync(id);
-
-            if (todoItem == null)
-            {
-                return NotFound(); 
-            }
-
-            return Ok(ItemToDTO(todoItem)); 
-        }
-
-        // PUT: api/TodoItems/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTodoItem(long id, TodoItemDTO todoDTO)
-        {
-            if (id != todoDTO.Id)
-            {
-                return BadRequest("ID mismatch."); 
-            }
-
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound(); 
-            }
-
-            
-            todoItem.Name = todoDTO.Name;
-            todoItem.IsComplete = todoDTO.IsComplete;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
-            {
-                return NotFound(); 
-            }
-
-            return NoContent(); 
-        }
-
-       
-        [HttpPost]
-        public async Task<ActionResult<TodoItemDTO>> PostTodoItem(TodoItemDTO todoDTO)
-        {
-            var todoItem = new TodoItem
-            {
-                IsComplete = todoDTO.IsComplete,
-                Name = todoDTO.Name
-            };
-
-            _context.TodoItems.Add(todoItem);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(
-                nameof(GetTodoItem),
-                new { id = todoItem.Id },
-                ItemToDTO(todoItem)); 
-        }
-
-        
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTodoItem(long id)
-        {
-            var todoItem = await _context.TodoItems.FindAsync(id);
-            if (todoItem == null)
-            {
-                return NotFound(); 
-            }
-
-            _context.TodoItems.Remove(todoItem);
-            await _context.SaveChangesAsync();
-
-            return NoContent(); 
-        }
-
-        private bool TodoItemExists(long id)
-        {
-            return _context.TodoItems.Any(e => e.Id == id);
-        }
-
-        private static TodoItemDTO ItemToDTO(TodoItem todoItem) =>
-           new TodoItemDTO
-           {
-               Id = todoItem.Id,
-               Name = todoItem.Name,
-               IsComplete = todoItem.IsComplete
-           };
+        _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ResponseDTO>>> GetResponses()
+    {
+        return await _context.Responses
+            .Select(x => ResponseToDTO(x))
+            .ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ResponseDTO>> GetResponse(long id)
+    {
+        var response = await _context.Responses.FindAsync(id);
+
+        if (response == null)
+        {
+            return NotFound();
+        }
+
+        return ResponseToDTO(response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutResponse(long id, ResponseDTO responseDTO)
+    {
+        if (id != responseDTO.Id)
+        {
+            return BadRequest();
+        }
+
+        var response = await _context.Responses.FindAsync(id);
+        if (response == null)
+        {
+            return NotFound();
+        }
+
+        response.Answer = responseDTO.Answer;
+        response.QuestionId = responseDTO.QuestionId;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!ResponseExists(id))
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResponseDTO>> PostResponse(ResponseDTO responseDTO)
+    {
+        var response = new Response
+        {
+            Answer = responseDTO.Answer,
+            QuestionId = responseDTO.QuestionId
+        };
+
+        _context.Responses.Add(response);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetResponse),
+            new { id = response.Id },
+            ResponseToDTO(response));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteResponse(long id)
+    {
+        var response = await _context.Responses.FindAsync(id);
+        if (response == null)
+        {
+            return NotFound();
+        }
+
+        _context.Responses.Remove(response);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ResponseExists(long id)
+    {
+        return _context.Responses.Any(e => e.Id == id);
+    }
+
+    private static ResponseDTO ResponseToDTO(Response response) =>
+       new ResponseDTO
+       {
+           Id = response.Id,
+           Answer = response.Answer,
+           QuestionId = response.QuestionId
+       };
 }
