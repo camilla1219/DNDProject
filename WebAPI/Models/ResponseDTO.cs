606b299a77ed66c187204df35746a128b092e6ee
liamDND <316346@via.dk>
2024-10-19 20:30:30 +0200

    ResponseDTO.cs
    Update

--- WebAPI/Models/ResponseDTO.cs
+++ WebAPI/Models/ResponseDTO.cs
@@ -1,9 +1,11 @@
-namespace TodoApi.Models
+namespace DNDProject.Models
 {
     public class ResponseDTO
     {
         public long Id { get; set; }
         public string Answer { get; set; } = string.Empty;
         public long QuestionId { get; set; }
+        public long SurveyId { get; set; }  
+        public DateTime ResponseDate { get; set; }  
     }
 }
