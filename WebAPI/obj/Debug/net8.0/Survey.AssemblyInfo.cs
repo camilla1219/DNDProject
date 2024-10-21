0d6760bfe377c6da0e1fccabcbed65097731395c
JacobEBS <Jacob_due@hotmail.com>
2024-10-21 17:03:50 +0200

    Looking at survey controller errors
    Found that there are errors due to the surveycontroller inheriting from controllerbase while using things that dont exist within that base

--- WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Survey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e695630a051428bfd9bef53dd9996302a65f278")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8df8aafcc1e8d52810306658de53d81ac85789")]
 [assembly: System.Reflection.AssemblyProductAttribute("Survey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Survey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
