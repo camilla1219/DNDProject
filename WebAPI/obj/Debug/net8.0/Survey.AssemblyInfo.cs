6ef3eb5273420fe7118b1af8cb561ff2047a609e
JacobEBS <Jacob_due@hotmail.com>
2024-10-21 16:53:35 +0200

    Looking at responseController
    Looking at the responsecontroller errors

--- WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Survey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5267de27ca69931685ec53c85ba80a9c9463d1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e695630a051428bfd9bef53dd9996302a65f278")]
 [assembly: System.Reflection.AssemblyProductAttribute("Survey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Survey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
