fc0f663e477c53fbfdb41f9e41c501bb6ca617e3
Camilla Richards <91956245+camilla1219@users.noreply.github.com>
2024-11-29 12:35:04 -0800

    Building it?

--- DNDBlazorApp/obj/Debug/net8.0/DNDBlazorApp.AssemblyInfo.cs
+++ DNDBlazorApp/obj/Debug/net8.0/DNDBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d27ed52259acc1eb3889a75355dfe74e14c6a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
