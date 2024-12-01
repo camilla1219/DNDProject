f0e9e77d79f0d69c15a62a88969eeccbf8744fe2
liamDND <316346@via.dk>
2024-12-01 23:35:23 +0100

    made a functionnal ViewSurvey.razor
    ViewSurvey.razor is now functionnal, linked the webapi to the blazor app

--- DNDBlazorApp/Program.cs
+++ DNDBlazorApp/Program.cs
@@ -2,22 +2,29 @@ using DNDBlazorApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Add HttpClient for API communication
+builder.Services.AddHttpClient("WebAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/"); // Update this URL to match your WebAPI's address
+});
+
+// Register HttpClient as scoped to use in Razor components
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WebAPI"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
--- WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Survey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ff908b0c9dcbf5a9503c5d7fb90053641194ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8082efd116ddc4d3fa9f5598c199c63592b6b185")]
 [assembly: System.Reflection.AssemblyProductAttribute("Survey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Survey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
