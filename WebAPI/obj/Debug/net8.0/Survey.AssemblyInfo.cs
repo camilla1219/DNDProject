cd07c92417a649ebc866bd70c2456c088dfbc983
Camilla Richards <91956245+camilla1219@users.noreply.github.com>
2024-12-01 18:21:34 -0800

    idk

--- WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/Survey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Survey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89828ed7cd2db92f92d672fd4837087a3a4805d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2a196e91324e392abff9ab16236741a24d9041")]
 [assembly: System.Reflection.AssemblyProductAttribute("Survey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Survey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
