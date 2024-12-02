0c3a9d7b78595cb453525502a13eea48b81bbd2d
Camilla Richards <91956245+camilla1219@users.noreply.github.com>
2024-12-01 18:32:34 -0800

    chsange toi survey

--- DNDBlazorApp/obj/Debug/net8.0/DNDBlazorApp.AssemblyInfo.cs
+++ DNDBlazorApp/obj/Debug/net8.0/DNDBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8082efd116ddc4d3fa9f5598c199c63592b6b185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e9e77d79f0d69c15a62a88969eeccbf8744fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNDBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
